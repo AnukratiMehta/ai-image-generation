10055159dfd3d0a7b2cc9233b0766c263a0f442d
thephenomenonu <108215785+thephenomenonu@users.noreply.github.com>
2023-02-10 01:03:46 +0530

    fixed formatting

--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -5,8 +5,8 @@ const connectDB = (url) => {
     mongoose.set('strictQuery', true);
 
     mongoose.connect(url)
-    .then(() => console.log('MongoDB connected'))
-    .catch((err) => console.log(err));
+        .then(() => console.log('MongoDB connected'))
+        .catch((err) => console.log(err));
 }
 
 export default connectDB;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <section className='bg-white py-2 border-t border-t-[#e6ebf4]'>
-      <p className='text-center text-[#666e75] text-sm'>
-        Built with <span className="heart" style={{color: "#FF10F0"}}>&#10084;</span> by Anukrati Mehta
-      </p>
-      <div className="text-center text-[#666e75]">
-        <a target="_blank" rel="noreferrer" href='https://github.com/AnukratiMehta/ai-image-generation.git' className="text-sm hover:text-[#FF10F0]">Source code</a>
-      </div>
-    </section>
-  )
+    return (
+        <section className='bg-white py-2 border-t border-t-[#e6ebf4]'>
+            <p className='text-center text-[#666e75] text-sm'>
+                Built with <span className="heart" style={{ color: "#FF10F0" }}>&#10084;</span> by Anukrati Mehta
+            </p>
+            <div className="text-center text-[#666e75]">
+                <a target="_blank" rel="noreferrer" href='https://github.com/AnukratiMehta/ai-image-generation.git' className="text-sm hover:text-[#FF10F0]">Source code</a>
+            </div>
+        </section>
+    )
 }
 
 export default Footer
