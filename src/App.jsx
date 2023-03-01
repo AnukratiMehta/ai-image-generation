66d672227dacbb6c586d53a1446c3ea3231ff405
thephenomenonu <108215785+thephenomenonu@users.noreply.github.com>
2023-03-01 16:58:14 +0530

    fixed home page

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Footer from './components/Footer';
 const App = () => (
   <BrowserRouter>
     <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
-      <Link to="/">
+      <Link to="/ai-image-generation">
         <img src={logo} alt="logo" className="w-28 object-contain" />
       </Link>
 
